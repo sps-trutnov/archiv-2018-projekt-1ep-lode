659c02b1bd89e845e0539777cc54c10615b8aaed
Jan Šprinc <sprinc@spstrutnov.cz>
2019-05-17 10:40:51 +0200

    Vybrani Policka

--- Lode/Program.cs
+++ Lode/Program.cs
@@ -1,11 +1,36 @@
-﻿namespace Lode
+﻿using System;
+namespace Lode
 {
     class Program
     {
         static void Main(string[] args)
         {
             //new Hra().SpustitHru();
-            
+            //ZACATEK 
+            var policko = Console.ReadLine();
+            //bool testovniString = true;
+            string ZakazanaPismena = "KLMNOPQRSTUVWXYZ";
+             
+            string Zasah = "A4"; // Policko vybrano hracem
+            while (policko.Contains(ZakazanaPismena))
+            {
+                Console.WriteLine("Tak to teda ne.");
+                Console.WriteLine("Zkus to znovu");
+                policko = Console.ReadLine();
+                
+            }
+                if (policko == Zasah)
+                {
+                    System.Console.WriteLine(StavPolicka.Zasah);
+                    System.Console.ReadKey(true);
+                }
+                else
+                {
+                    Console.WriteLine(StavPolicka.Mimo);
+                    Console.ReadKey(true);
+                }
+                
         }
+            
     }
 }
