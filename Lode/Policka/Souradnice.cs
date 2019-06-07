68bcc26a72d752b57b68aa5d5ea68557808d7b9b
Jakub Šenkýř <senkyr@spstrutnov.cz>
2019-06-07 13:02:05 +0200

    Doplněný konstruktor Souradnice

--- Lode/Policka/Souradnice.cs
+++ Lode/Policka/Souradnice.cs
@@ -4,5 +4,15 @@
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        public Souradnice()
+        {
+
+        }
+        public Souradnice(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
