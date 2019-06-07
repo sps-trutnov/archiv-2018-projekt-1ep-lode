161f241b777359bc433d6ca4b5eba13d2124945b
Poliak <poliak@spstrutnov.cz>
2019-06-07 13:22:46 +0200

    Zakladni pohyb lodi

--- Lode/Program.cs
+++ Lode/Program.cs
@@ -6,44 +6,52 @@ namespace Lode
         static void Main(string[] args)
         {
             bool jeNaTahu = true;
-            int y = 0;
-            int x = 0;
-
+            int y = 1;
+            int x = 1;
+            int[] herniPoleX = new int[10];
+            int[] herniPoleY = new int[10];
             //new Hra(new TextoveRozhrani()).SpustitHru();
             while (jeNaTahu)
             {
-
-                if (Console.ReadKey(true).KeyChar == 'd' && x <= 10)
+                Console.CursorVisible = false;
+                if (Console.ReadKey(true).KeyChar == 'd' && x <= 10-1)
                 {
                     x++;
+                    
                     Console.Clear();
                 }
 
-                else if (Console.ReadKey(true).KeyChar == 's' && y <= 10 )
+                else if (Console.ReadKey(true).KeyChar == 's' && y <= 10-1 )
                 {
                     y++;
                     Console.Clear();
                 }
-                else if (Console.ReadKey(true).KeyChar == 'w' && y >= 0 || y == 10)
+                else if (Console.ReadKey(true).KeyChar == 'w' && y > 1 || y == 10-1)
                 {
                     y--;
                     Console.Clear();
                 }
-                else if (Console.ReadKey(true).KeyChar == 'a' && x >= 0 || x == 10)
+                else if (Console.ReadKey(true).KeyChar == 'a' && x > 1 || x == 10-1)
                 {
                     x--;
                     Console.Clear();
                 }
                 nakresliPole();
                 Console.SetCursorPosition(x, y);
-                
                 Console.Write('X');
+                Console.SetCursorPosition(x - 1, y);
+                Console.Write('X');
+                Console.SetCursorPosition(x + 1, y);
+                Console.Write('X');
+                Console.SetCursorPosition(x, y - 1);
+                Console.Write('X');
+
             }
             void nakresliPole()
             {
-                for (int i = 0; i < 10; i++)
+                for (int i = 0; i < herniPoleY.Length; i++)
                 {
-                    for (int l = 0; l < 10; l++)
+                    for (int l = 0; l < herniPoleY.Length; l++)
                     {
                         Console.SetCursorPosition(l, i);
                         Console.Write("*");
