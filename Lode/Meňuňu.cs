37b62031c3eff243bc400641373071dafa339d8a
SaiNs <plihal.spstrutnov.cz>
2019-06-07 13:20:25 +0200

    Nadpis menu

---
+++ Lode/Meňuňu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lode
+{
+    class Meňuňu
+    {
+        public void UvodniMenu()
+        {
+            Console.SetWindowSize (120, 35);
+            Console.WriteLine("## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##");
+            Console.WriteLine(" ## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##");
+            Console.WriteLine();
+            Console.WriteLine(" ####        ####     ########       ##########             ## ##                                  ##   ##");
+            Console.WriteLine(" ####      ###  ###   ####  ####     ###                   #########                              #   #   #");
+            Console.WriteLine(" ####     ###    ###  ####   ####    ###                   ###########         ###### ");
+            Console.WriteLine(" ####     ###    ###  ####    ####   ##########         ###################################");
+            Console.WriteLine(" ####     ###    ###  ####   ####    ###                ### ## ## ## ## ## ## ## ## ##### ");
+            Console.WriteLine(" ####      ###  ###   ####  ####     ###                ################################");
+            Console.WriteLine(" ########    ####     ########       ##########          #############################");
+            Console.WriteLine();
+            Console.WriteLine(" ## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##");
+            Console.WriteLine("## # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##");
+            Console.ReadKey();
+        }
+    }
+}
--- Lode/Program.cs
+++ Lode/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            new Hra(new TextoveRozhrani()).SpustitHru();
+            Meňuňu m = new Meňuňu();
+            m.UvodniMenu();
         }
     }
 }
