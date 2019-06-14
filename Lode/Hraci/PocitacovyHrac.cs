f52a86871e948367a796fa5895dbc9abd291002e
PowerFilip <povr@spstrutnov.cz>
2019-06-14 10:41:30 +0200

    Rozmisteni Lodi

--- Lode/Hraci/PocitacovyHrac.cs
+++ Lode/Hraci/PocitacovyHrac.cs
@@ -9,6 +9,7 @@ namespace Lode
         readonly private static string[] _jmenaAI = new string[] { "Andy", "Boris", "Dora", "Keira", "Victor" };
 
         public string Jmeno { get; private set; }
+        public object StavPolicka { get; private set; }
 
         public PocitacovyHrac() : base(new IPAddress(new byte[] { 127, 0, 0, 1 }))
         {
@@ -24,14 +25,41 @@ namespace Lode
             Random y = new Random();
             int souradniceY = y.Next(1, 10);
 
+            Console.WriteLine(souradniceX);
+            Console.WriteLine(souradniceY);
+
             return new Souradnice() { X = souradniceX, Y = souradniceY };
+
 
         }
         public override void RozmistitLode()
         {
             this.HerniPole = new StavPolicka[10, 10];
 
+            Random x = new Random();
+            int souradniceX = x.Next(1, 10);
+            System.Threading.Thread.Sleep(1000);
+            Random y = new Random();
+            int souradniceY = y.Next(1, 10)
 
+            TypLode MojeLod = TypLode.Torpedovka;
+            NatoceniLode Nulovy_Stupen = NatoceniLode.Stupnu0;
+            Souradnice centrum = new Souradnice() { X = souradniceX, Y = souradniceY };
+
+            if ((MojeLod == TypLode.Torpedovka) && (Nulovy_Stupen == NatoceniLode.Stupnu0))
+             {
+                Souradnice vlevoOdCentra = new Souradnice() { X = souradniceX - 1, Y = souradniceY };
+                Souradnice nahoreOdCentra = new Souradnice() { X = souradniceX, Y = souradniceY + 1 };
+                Souradnice vpravoOdCentra = new Souradnice() { X = souradniceX + 1, Y = souradniceY };
+
+                Console.WriteLine(vlevoOdCentra);
+                Console.WriteLine(nahoreOdCentra);
+                Console.WriteLine(vpravoOdCentra);
+
+
+
+            }
+            Console.ReadLine();
         }
     }
 }
