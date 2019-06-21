f9c60a5a0a407655f06d6b58e3316ac211741eb3
Jan Pchálek <pchalek@spstrutnov.cz>
2019-06-21 13:09:38 +0200

    tym2

--- Lode/Hraci/ObecnyHrac.cs
+++ Lode/Hraci/ObecnyHrac.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Lode
 {
@@ -20,10 +21,13 @@ namespace Lode
         protected Socket PrijimaciKomunikacniKanal { get; set; }
 
         protected bool ZahajujeKomunikaci;
+        private byte zasilka1;
+        private byte zasilka2;
 
         public StavPolicka[,] HerniPole { get; protected set; }
         public StavPolicka[,] HerniPoleSoupere { get; protected set; }
         public List<Lod> Lode { get; protected set; }
+        public int ipAddress { get; private set; }
 
         public ObecnyHrac(IPAddress vlastniAdresa)
         {
@@ -35,7 +39,11 @@ namespace Lode
 
         public int GenerovatToken()
         {
-            throw new NotImplementedException();
+            Random rnd = new Random();
+            int token = rnd.Next(1, 2);
+            return token;
+
+            
         }
         public bool JePorazenym()
         {
@@ -43,15 +51,61 @@ namespace Lode
         }
         public bool MaPravoPrvnihoTahu()
         {
-            throw new NotImplementedException();
+            GenerovatToken();
+            VymenitSiTokenSeSouperem();
+            if (ipAddress > AdresaSoupere())
+            {
+                if (zasilka1 > zasilka2)
+
+                return true;
+
+                else
+
+                return false;
+            }
         }
         public void NastavitAdresuSoupere(IPAddress adresaSoupere)
-        {
-            throw new NotImplementedException();
+        {   
+            
+            AdresaSoupere = adresaSoupere;
         }
         public void NavazatSpojeniSeSouperem()
         {
-            throw new NotImplementedException();
+            
+            IPAddress ipAddress = new IPAddress(new byte[] { 192, 168, 216, 207 });
+            IPEndPoint IpPort = new IPEndPoint(ipAddress, 11000);
+
+            if (ipAddress > AdresaSoupere())
+            {
+                Socket Posta = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    Posta.Connect(IpPort);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        Posta.RemoteEndPoint.ToString());
+
+                    byte[] msg = Encoding.UTF8.GetBytes("tady máte zásilku");
+                }
+            }
+            else
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+
+                try
+                {
+                    Socket klient = VysilaciKomunikacniKanal;
+                    //Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    klient.Connect(localEndPoint);
+
+                    Console.WriteLine("Waiting for a connection...");
+                }
+            }
         }
         public bool NemuzeProvestDalsiTah()
         {
@@ -73,17 +127,46 @@ namespace Lode
         {
             throw new NotImplementedException();
         }
-        public int VymenitSiTokenSeSouperem(int vlastniToken)
-        {
-            throw new NotImplementedException();
+        public int VymenitSiTokenSeSouperem(int token)
+        {   
+
+            if (ipAddress > AdresaSoupere())
+            {
+                byte[] zasilka1 = BitConverter.GetBytes(token);
+                Socket postak = VysilaciKomunikacniKanal;
+                postak.Send(zasilka1);
+                postak.Close();
+                postak.Shutdown(SocketShutdown.Both);
+                postak.Receive(zasilka2);
+                
+
+
+            }
+            else
+            {
+                byte[] zasilka2 = BitConverter.GetBytes(token);
+                Socket postak = VysilaciKomunikacniKanal;
+                postak.Send(zasilka2);
+                postak.Close();
+                postak.Shutdown(SocketShutdown.Both);
+                postak.Receive(zasilka1);
+            }
         }
         public Souradnice ZjistitTahSoupere()
         {
-            throw new NotImplementedException();
+            byte[] dataX = BitConverter.GetBytes(tah.X);
+            byte[] dataY = BitConverter.GetBytes(tah.Y);
+            postak.Send(dataX);
+            postak.Send(dataY);
+            postak.Close();
+            postak.Shutdown(SocketShutdown.Both);
+     
         }
         public StavPolicka ZjistitVysledekTahuOdSoupere(Souradnice tah)
         {
-            throw new NotImplementedException();
+            postak.Receive(dataX);
+            postak.Receive(dataY);
+
         }
     }
 }
