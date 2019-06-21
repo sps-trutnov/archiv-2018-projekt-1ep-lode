14108cb5b7633b0b3b31f50e98e2f725e617248d
Jan Pchálek <pchalek@spstrutnov.cz>
2019-06-21 13:09:38 +0200

    Připojení mezi hráči, Posílání dat

--- Lode/Hraci/ObecnyHrac.cs
+++ Lode/Hraci/ObecnyHrac.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Lode
 {
@@ -19,15 +20,15 @@ namespace Lode
         protected Socket VysilaciKomunikacniKanal { get; set; }
         protected Socket PrijimaciKomunikacniKanal { get; set; }
 
-        protected bool ZahajujeKomunikaci;
-
         public StavPolicka[,] HerniPole { get; protected set; }
         public StavPolicka[,] HerniPoleSoupere { get; protected set; }
         public List<Lod> Lode { get; protected set; }
+        IPAddress ipAddress = new IPAddress(new byte[] { 192, 168, 216, 207 });
+        IPEndPoint IpPort = new IPEndPoint(ipAddress, 11000);
 
         public ObecnyHrac(IPAddress vlastniAdresa)
         {
-            
+
         }
 
         public abstract Souradnice RozhodnoutVlastniTah();
@@ -35,7 +36,11 @@ namespace Lode
 
         public int GenerovatToken()
         {
-            throw new NotImplementedException();
+            Random rnd = new Random();
+            int token = rnd.Next(1, 2);
+            return token;
+
+
         }
         public bool JePorazenym()
         {
@@ -43,15 +48,168 @@ namespace Lode
         }
         public bool MaPravoPrvnihoTahu()
         {
-            throw new NotImplementedException();
+            int token = GenerovatToken();
+            VymenitSiTokenSeSouperem(token);
+            if (ipAddress > AdresaSoupere)
+            {
+                if (zasilka1 > zasilka2)
+
+                    return true;
+
+                else
+
+                    return false;
+            }
         }
         public void NastavitAdresuSoupere(IPAddress adresaSoupere)
         {
-            throw new NotImplementedException();
+
+            AdresaSoupere = adresaSoupere;
         }
+        int Hosting = 2;
+        public bool MojeAdresaJeVetsi(string mojeAdresa, string jehoAdresa)
+        {
+            bool sesHost = false;
+            bool jedemDal = false;
+            bool souperJeHost = false;
+            string ip = mojeAdresa;
+            string[] tokens = ip.Split('.');
+            int value1_1 = Int32.Parse(tokens[0]);
+            int value1_2 = Int32.Parse(tokens[1]);
+            int value1_3 = Int32.Parse(tokens[2]);
+            int value1_4 = Int32.Parse(tokens[3]);
+            Console.WriteLine(value1_1);
+            Console.WriteLine(value1_2);
+            Console.WriteLine(value1_3);
+            Console.WriteLine(value1_4);
+            string ip2 = jehoAdresa;
+            string[] tokens2 = ip2.Split('.');
+            int value2_1 = Int32.Parse(tokens2[0]);
+            int value2_2 = Int32.Parse(tokens2[1]);
+            int value2_3 = Int32.Parse(tokens2[2]);
+            int value2_4 = Int32.Parse(tokens2[3]);
+            Console.WriteLine(value2_1);
+            Console.WriteLine(value2_2);
+            Console.WriteLine(value2_3);
+            Console.WriteLine(value2_4);
+            if (value1_1 == value2_1)
+            {
+                jedemDal = true;
+            }
+            else
+            {
+                if (value1_1 > value2_1)
+                {
+                    sesHost = true;
+                }
+                else
+                {
+                    souperJeHost = true;
+                }
+            }
+            if (jedemDal == true)
+            {
+                jedemDal = false;
+                if (value1_2 == value2_2)
+                {
+                    jedemDal = true;
+                }
+                else
+                {
+                    if (value1_2 > value2_2)
+                    {
+                        sesHost = true;
+                    }
+                    else
+                    {
+                        souperJeHost = true;
+                    }
+                }
+            }
+            if (jedemDal == true)
+            {
+                jedemDal = false;
+                if (value1_3 == value2_3)
+                {
+                    jedemDal = true;
+                }
+                else
+                {
+                    if (value1_3 > value2_3)
+                    {
+                        sesHost = true;
+                    }
+                    else
+                    {
+                        souperJeHost = true;
+                    }
+                }
+            }
+            if (jedemDal == true)
+            {
+                if (value1_4 == value2_4)
+                {
+                    Console.WriteLine("DVĚ STEJNÝ IP ADRESY?! CO MI O TOM JAKO ŘEKNEŠ? HMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM?");
+                }
+                else
+                {
+                    if (value1_4 > value2_4)
+                    {
+                        sesHost = true;
+                    }
+                    else
+                    {
+                        souperJeHost = true;
+                    }
+                }
+            }
+            if (sesHost == true)
+            {
+                return true;
+            }
+
+            if (souperJeHost == true)
+            {
+                return false;
+            }
+
+            throw new Exception("Uplay stoped working - ERROR 401");
+        }   
         public void NavazatSpojeniSeSouperem()
         {
-            throw new NotImplementedException();
+            bool zacinam = MojeAdresaJeVetsi(VlastniAdresa.ToString(), AdresaSoupere.ToString());
+
+
+            if (zacinam)
+            {
+                Socket Posta = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+
+                Posta.Connect(IpPort);
+
+                Console.WriteLine("Socket connected to {0}");
+                Posta.RemoteEndPoint.ToString();
+
+                byte[] msg = Encoding.UTF8.GetBytes("tady máte zásilku");
+
+            }
+            else
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+
+
+                Socket klient = VysilaciKomunikacniKanal;
+                //Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                klient.Connect(localEndPoint);
+
+                Console.WriteLine("Waiting for a connection...");
+
+            }
         }
         public bool NemuzeProvestDalsiTah()
         {
@@ -73,17 +231,46 @@ namespace Lode
         {
             throw new NotImplementedException();
         }
-        public int VymenitSiTokenSeSouperem(int vlastniToken)
+        public int VymenitSiTokenSeSouperem(int token)
         {
-            throw new NotImplementedException();
+
+            if (ipAddress > AdresaSoupere())
+            {
+                byte[] zasilka1 = BitConverter.GetBytes(token);
+                Socket postak = VysilaciKomunikacniKanal;
+                postak.Send(zasilka1);
+                postak.Close();
+                postak.Shutdown(SocketShutdown.Both);
+                postak.Receive(zasilka2);
+
+
+
+            }
+            else
+            {
+                byte[] zasilka2 = BitConverter.GetBytes(token);
+                Socket postak = VysilaciKomunikacniKanal;
+                postak.Send(zasilka2);
+                postak.Close();
+                postak.Shutdown(SocketShutdown.Both);
+                postak.Receive(zasilka1);
+            }
         }
         public Souradnice ZjistitTahSoupere()
         {
-            throw new NotImplementedException();
+            byte[] dataX = BitConverter.GetBytes(tah.X);
+            byte[] dataY = BitConverter.GetBytes(tah.Y);
+            postak.Send(dataX);
+            postak.Send(dataY);
+            postak.Close();
+            postak.Shutdown(SocketShutdown.Both);
+
         }
         public StavPolicka ZjistitVysledekTahuOdSoupere(Souradnice tah)
         {
-            throw new NotImplementedException();
+            postak.Receive(dataX);
+            postak.Receive(dataY);
+
         }
     }
 }
--- Lode/Program.cs
+++ Lode/Program.cs
@@ -18,53 +18,58 @@ namespace Lode
             StartServer();
             new Hra(new TextoveRozhrani()).SpustitHru();
         }
-
-
         public static void StartServer()
         {
             // Get Host IP Address that is used to establish a connection  
             // In this case, we get one IP address of localhost that is IP : 127.0.0.1  
             // If a host has multiple addresses, you will get a list of addresses  
-            IPHostEntry host = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = host.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            IPAddress ipAddress = new IPAddress(new byte[] { 192, 168, 216, 207 });
+            IPEndPoint pripojka = new IPEndPoint(ipAddress, 11000);
+            Console.WriteLine("Poslouchám na IP adrese " + ipAddress.ToString());
 
 
             try
             {
 
                 // Create a Socket that will use Tcp protocol      
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Socket trubka = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
                 // A Socket must be associated with an endpoint using the Bind method  
-                listener.Bind(localEndPoint);
+                trubka.Bind(pripojka);
                 // Specify how many requests a Socket can listen before it gives Server busy response.  
                 // We will listen 10 requests at a time  
-                listener.Listen(10);
+                trubka.Listen(10);
 
                 Console.WriteLine("Waiting for a connection...");
-                Socket handler = listener.Accept();
+                Socket pripojenatrubka = trubka.Accept();
 
                 // Incoming data from the client.    
-                string data = null;
-                byte[] bytes = null;
+                string prijatyText = null;
+                byte[] buffer = null;
 
                 while (true)
                 {
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    if (data.IndexOf("<EOF>") > -1)
+                    buffer = new byte[1024];
+                    int pocetPrijatychBytu = pripojenatrubka.Receive(buffer);
+                    prijatyText += Encoding.UTF8.GetString(buffer, 0, pocetPrijatychBytu);
+                    Console.WriteLine("Zatím jsem obdržel text: " + prijatyText);
+                    /*Console.WriteLine("Napiš end pro konec");
+                    if (prijatyText == "end")
+                    {
+                        break;
+                    }
+                    */
+                    if (prijatyText.IndexOf("hotovo") > -1)
                     {
                         break;
                     }
                 }
 
-                Console.WriteLine("Text received : {0}", data);
+                Console.WriteLine("Text received : {0}", prijatyText);
 
-                byte[] msg = Encoding.ASCII.GetBytes(data);
-                handler.Send(msg);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
+                byte[] msg = Encoding.UTF8.GetBytes(prijatyText);
+                pripojenatrubka.Send(msg);
+                pripojenatrubka.Shutdown(SocketShutdown.Both);
+                pripojenatrubka.Close();
             }
             catch (Exception e)
             {
