5e4562aa13023cd98b922f21b03b7b0d44515d96
SaiNs <plihal@spstrutnov.cz>
2019-06-25 11:50:19 +0200

    Základy posuvných CREDITS

--- Lode/Menu/About.cs
+++ Lode/Menu/About.cs
@@ -8,59 +8,96 @@ namespace Lode
 {
     class About
     {
+        string[] rolovaciText = new string[] {
+        "##########################################################################################################",
+        "                                            SPŠ TRUTNOV                                                   ",
+        "                                         #################                                                ",
+        "                                                                                                          ",
+        "                                             SENSEI                                                       ",
+        "                                          ############                                                    ",
+        "                                                                                                          ",
+        "                                          ŠENKÝŘ JAKUB                                                    ",
+        "                                                                                                          ",
+        "                                           TÝM 1 - A.I.                                                   ",
+        "                                         ################                                                 ",
+        "                                                                                                          ",
+        "                                            POVR FILIP                                                    ",
+        "                                        ŠTĚPÁNEK DOMINIK                                                  ",
+        "                                         ŽATECKÝ DUŠAN                                                    ",
+        "                                                                                                          ",
+        "                                      TÝM 2 - PROPOJENÍ HRÁČŮ                                             ",
+        "                                    ###########################                                           ",
+        "                                                                                                          ",
+        "                                          BINAR DAVID                                                     ",
+        "                                        MLEJNEK STANISLAV                                                 ",
+        "                                        PCHÁLEK JAN                                                       ",
+        "                                                                                                          ",
+        "                                     TÝM 3 - PROVEDENÍ TAHŮ                                               ",
+        "                                   ##########################                                             ",
+        "                                                                                                          ",
+        "                                          PETRŮ JAN                                                       ",
+        "                                           RODR MICHAL                                                    ",
+        "                                         ŠPRINC JAN                                                       ",
+        "                                                                                                          ",
+        "                                    TÝM 4 - ROZMÍSTENÍ LODÍ                                               ",
+        "                                  ###########################                                             ",
+        "                                                                                                          ",
+        "                                         POLIAK TADEÁŠ                                                    ",
+        "                                          SYNEK MARTIN                                                    ",
+        "                                       ŠPRINGER DAVID                                                     ",
+        "                                                                                                          ",
+        "                                        TÝM 5 - DESIGN                                                    ",
+        "                                      ##################                                                  ",
+        "                                                                                                          ",
+        "                                       PETRŽELA ONDŘEJ                                                    ",
+        "                                         PLÍHAL MARTIN                                                    ",
+        "                                          PRESL MARTIN                                                    ",
+        "                                                                                                          ",
+        "                     (C) Ikonu vytvořil Freepik z webu www.flaticon.com                                   ",
+        "##########################################################################################################",
+        "                                                                                                          ",
+        "Pro vrácení zpět stiskni libovolné tlačítko.                                                              ",
+            };
+
         public void Credits()
         {
             Console.Clear();
-            Console.WriteLine("##########################################################################################################");
-            Console.WriteLine("                                            SPŠ TRUTNOV");
-            Console.WriteLine("                                         #################");
-            Console.WriteLine();
-            Console.WriteLine("                                             SENSEI");
-            Console.WriteLine("                                          ############");
-            Console.WriteLine();
-            Console.WriteLine("                                          ŠENKÝŘ JAKUB");
-            Console.WriteLine();
-            Console.WriteLine("                                           TÝM 1 - A.I.");
-            Console.WriteLine("                                         ################");
-            Console.WriteLine();
-            Console.WriteLine("                                            POVR FILIP");
-            Console.WriteLine("                                        ŠTĚPÁNEK DOMINIK");
-            Console.WriteLine("                                         ŽATECKÝ DUŠAN");
-            Console.WriteLine();
-            Console.WriteLine("                                      TÝM 2 - PROPOJENÍ HRÁČŮ");
-            Console.WriteLine("                                    ###########################");
-            Console.WriteLine();
-            Console.WriteLine("                                          BINAR DAVID");
-            Console.WriteLine("                                        MLEJNEK STANISLAV");
-            Console.WriteLine("                                        PCHÁLEK JAN");
-            Console.WriteLine();
-            Console.WriteLine("                                     TÝM 3 - PROVEDENÍ TAHŮ");
-            Console.WriteLine("                                   ##########################");
-            Console.WriteLine();
-            Console.WriteLine("                                          PETRŮ JAN");
-            Console.WriteLine("                                           RODR MICHAL");
-            Console.WriteLine("                                         ŠPRINC JAN");
-            Console.WriteLine();
-            Console.WriteLine("                                    TÝM 4 - ROZMÍSTENÍ LODÍ");
-            Console.WriteLine("                                  ###########################");
-            Console.WriteLine();
-            Console.WriteLine("                                         POLIAK TADEÁŠ");
-            Console.WriteLine("                                          SYNEK MARTIN");
-            Console.WriteLine("                                       ŠPRINGER DAVID");
-            Console.WriteLine();
-            Console.WriteLine("                                        TÝM 5 - DESIGN");
-            Console.WriteLine("                                      ##################");
-            Console.WriteLine();
-            Console.WriteLine("                                       PETRŽELA ONDŘEJ");
-            Console.WriteLine("                                         PLÍHAL MARTIN");
-            Console.WriteLine("                                          ̶PRESL MARTIN");
-            Console.WriteLine();
-            Console.WriteLine("                     (C) Ikonu vytvořil Freepik z webu www.flaticon.com");
-            Console.WriteLine("##########################################################################################################");
-            Console.WriteLine();
-            Console.WriteLine("Pro vrácení zpět stiskni libovolné tlačítko.");
-            Console.ReadKey();
-            Console.Clear();
+
+            int kolikRadkuVypsat = 1;
+            int vrsekVypisu = 35;
+
+            // faze 1 - najizdeni
+            while (vrsekVypisu > 0)
+            {
+                Console.CursorTop = vrsekVypisu;
+                Console.CursorLeft = 0;
+
+                for(int i = 0; i < kolikRadkuVypsat; i++)
+                    Console.WriteLine(rolovaciText[i]);
+
+                Console.ReadKey();
+
+                vrsekVypisu -= 1;
+                Console.CursorLeft = 0;
+                kolikRadkuVypsat += 1;
+            }
+
+            // faze 2 - prujezd
+            while (kolikRadkuVypsat <46)
+            {
+                vrsekVypisu = 0;
+                Console.CursorTop = vrsekVypisu;
+                Console.CursorLeft = 0;
+
+                for (int i = 35; i < kolikRadkuVypsat; i++)
+                    Console.WriteLine(rolovaciText[i]);
+                Console.ReadLine();
+
+                vrsekVypisu -= 1;
+                kolikRadkuVypsat += 1;
+            }
+            // faze 3 - dojezd
+
         }
     }
 }
