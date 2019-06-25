3e9bd6f7e6c0aafbd3c59b3eb2a7998f0913514c
DusanZatecky <zatecky@spstrutnov.cz>
2019-06-25 11:44:39 +0200

    Schváleno a doděláno :)

--- Lode/Hraci/PocitacovyHrac.cs
+++ Lode/Hraci/PocitacovyHrac.cs
@@ -17,79 +17,54 @@ namespace Lode
 
         public override Souradnice RozhodnoutVlastniTah()
         {
+            Random nahoda = new Random();
 
-        while(true)
-            {
-            Random x = new Random();
-            int souradniceX = x.Next(0, HerniPoleSoupere.GetLength(0));
-            System.Threading.Thread.Sleep(1000);
-            Random y = new Random();
-            int souradniceY = y.Next(0, HerniPoleSoupere.GetLength(1));
+            int souradniceX = nahoda.Next(0, HerniPoleSoupere.GetLength(0));
+            int souradniceY = nahoda.Next(0, HerniPoleSoupere.GetLength(1));
 
             while (HerniPoleSoupere[souradniceX, souradniceY] != StavPolicka.Neznamo)
             {
-                souradniceX = x.Next(0, HerniPoleSoupere.GetLength(0));
-                souradniceY = y.Next(0, HerniPoleSoupere.GetLength(1));
-            }
+                //
 
-            if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Zasah)
-            {
-                Random zasahy = new Random();
-                int souradniceStran = zasahy.Next(0, 4);
-
-                if (souradniceStran == 0 & HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
+                if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Zasah)
                 {
-                    souradniceY = souradniceY + 1;
+                    int moznostiSmeru = nahoda.Next(0, 4);
 
-                    if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
+                    if (moznostiSmeru == 0)
                     {
-                        return new Souradnice() { X = souradniceX, Y = souradniceY };
+                        souradniceY = souradniceY + 1;
+
+                        if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
+                            return new Souradnice() { X = souradniceX, Y = souradniceY };
+                    }
+                    else if (moznostiSmeru == 1)
+                    {
+                        souradniceX = souradniceX + 1;
+
+                        if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
+                            return new Souradnice() { X = souradniceX, Y = souradniceY };
+                    }
+                    else if (moznostiSmeru == 2)
+                    {
+                        souradniceY = souradniceY - 1;
+
+                        if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
+                            return new Souradnice() { X = souradniceX, Y = souradniceY };
+                    }
+                    else if (moznostiSmeru == 3)
+                    {
+                        souradniceX = souradniceX - 1;
+
+                        if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
+                            return new Souradnice() { X = souradniceX, Y = souradniceY };
                     }
                 }
-                else if (souradniceStran == 1 & HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
-                {
-                    souradniceX = souradniceX + 1;
 
-                    if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
-                    {
-                        return new Souradnice() { X = souradniceX, Y = souradniceY };
-                    }
-                }
-                else if (souradniceStran == 2 & HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
-                {
-                    souradniceY = souradniceY - 1;
-
-                    if(HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
-                    {
-                        return new Souradnice() { X = souradniceX, Y = souradniceY };
-                    }
-                }
-                else if (souradniceStran == 3)
-                {
-                    souradniceX = souradniceX - 1;
-
-                    if (HerniPoleSoupere[souradniceX, souradniceY] == StavPolicka.Neznamo)
-                    {
-                        return new Souradnice() { X = souradniceX, Y = souradniceY };
-                    }        
-                }
-
+                souradniceX = nahoda.Next(0, HerniPoleSoupere.GetLength(0));
+                souradniceY = nahoda.Next(0, HerniPoleSoupere.GetLength(1));
             }
 
-
-
-                Console.WriteLine(souradniceX);
-            Console.WriteLine(souradniceY);
-
-           
-                return new Souradnice() { X = souradniceX, Y = souradniceY };
-            }
-
-            
-            
-               
-
-
+            return new Souradnice() { X = souradniceX, Y = souradniceY };
         }
         public override void RozmistitLode()
         {
@@ -106,7 +81,7 @@ namespace Lode
             Souradnice centrum = new Souradnice() { X = souradniceX, Y = souradniceY };
 
             if ((MojeLod == TypLode.Torpedovka) && (Nulovy_Stupen == NatoceniLode.Stupnu0))
-             {
+            {
                 Souradnice vlevoOdCentra = new Souradnice() { X = souradniceX - 1, Y = souradniceY };
                 Souradnice nahoreOdCentra = new Souradnice() { X = souradniceX, Y = souradniceY + 1 };
                 Souradnice vpravoOdCentra = new Souradnice() { X = souradniceX + 1, Y = souradniceY };
@@ -114,7 +89,7 @@ namespace Lode
                 Console.WriteLine(vlevoOdCentra);
                 Console.WriteLine(nahoreOdCentra);
                 Console.WriteLine(vpravoOdCentra);
-                
+
 
 
             }
